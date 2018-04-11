cd7ea0a403b0c0894689dea1e9f8c0f720980e3d
LiuXPeng <whyLiuXPeng@users.noreply.github.com>
2018-04-11 16:57:23 +0800

    d

--- Preprocessing.py
+++ Preprocessing.py
@@ -9,25 +9,36 @@ from pandas import Series, DataFrame
 import numpy as np
 import pandas as pd
 
-x = pd.read_csv("uci-news-aggregator.csv")
-counts = x['CATEGORY'].value_counts()
-print(counts)
+def cleanData():
+	#去掉数据中不需要的属性，将多分类变成二分类问题
+	x = pd.read_csv("uci-news-aggregator.csv")
+	counts = x['CATEGORY'].value_counts()
+	print("整个数据集类别统计", counts)
 
-del x['ID']
-del x['URL']
-del x['PUBLISHER']
-del x['STORY']
-del x['HOSTNAME']
-del x['TIMESTAMP']
-#x.to_csv('a.csv', sep=',', header=None, index=None)
-x['CATEGORY'].replace(['b', 't', 'e', 'm'], [-1, 1, -1, -1],inplace = True)
-#print(x)
-#x.to_csv('data.csv', sep=',', header=None, index=None)
-counts = x['CATEGORY'].value_counts()
-print(counts)
-trainingData = x.sample(n = 100000)
-print(trainingData['CATEGORY'].value_counts())
-trainingData.insert(0, 'LABEL', trainingData['CATEGORY'])
-del trainingData['CATEGORY']
-print(trainingData)
-trainingData.to_csv('trainingData.txt', sep=',', header=None, index=None)
+	del x['ID']
+	del x['URL']
+	del x['PUBLISHER']
+	del x['STORY']
+	del x['HOSTNAME']
+	del x['TIMESTAMP']
+
+	x['CATEGORY'].replace(['b', 't', 'e', 'm'], [-1, 1, -1, -1],inplace = True)
+	#因为已经生成新的文件，因此这里将下面生成文件的代码注释
+	#print(x)
+	#x.to_csv('data.csv', sep=',', header=None, index=None)
+	counts = x['CATEGORY'].value_counts()
+	print("整个数据集二分类别统计", counts)
+	return None
+
+def sampleData(n = 100000):
+	#抽取数据做训练集或测试集
+	trainingData = x.sample(n)
+	print("统计抽取样本",trainingData['CATEGORY'].value_counts())
+
+	#这里将标签放在前面，文本放在后面
+	trainingData.insert(0, 'LABEL', trainingData['CATEGORY'])
+	del trainingData['CATEGORY']
+
+	trainingData.to_csv('trainingData.txt', sep=',', header=None, index=None)
+	return None
+
