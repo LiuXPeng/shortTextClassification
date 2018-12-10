1f312949c692342b363b35d23b4194be83b42ed0
LiuXPeng <703762445@qq.com>
2018-12-10 10:54:31 +0800

    lr函数有问题

--- LR.py
+++ LR.py
@@ -38,10 +38,7 @@ def lrTrain(lb = {'b':1, 't':1, 'e':1, 'm':-1}, n = 1000,  fe = 'one-hot', desce
 	if descend == 'lda':
 		x = fE.ldaGet(x)
 
-	clf = LinearRegression(penalty='l1', dual=False,
-         ‍tol=0.0001, C=1.0, fit_intercept=True, intercept_scaling=1, 
-         class_weight=“balanced”, random_state=None, solver='liblinear', 
-         max_iter=100, multi_class='ovr', verbose=0, warm_start=False, n_jobs=1)
+	clf = LinearRegression(penalty='l2', dual=False, tol=0.0001, C=1.0, fit_intercept=True, intercept_scaling=1, class_weight="balanced", random_state=None, solver='liblinear', max_iter=100, multi_class='ovr', verbose=0, warm_start=False, n_jobs=1)
 	clf.fit(x, y)
 	joblib.dump(clf, "lrTrainModel.m")
 
